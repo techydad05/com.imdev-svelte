e4a01e02b3a2f620b3c2be4bbc134d3a90f98b24
Ivan <75285470+techydad05@users.noreply.github.com>
2022-02-22 14:57:33 -0500

    fixing medusa deploy

--- src/lib/medusa-client.js
+++ src/lib/medusa-client.js
@@ -1,4 +1,4 @@
-import Medusa from "@medusajs/medusa-js";
+import { default as Medusa } from "@medusajs/medusa-js";
 
 const BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL || "http://localhost:9000";
 
