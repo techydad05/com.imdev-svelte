d5d0175eb64d7c5d6ff38e50e5ae3c784747498c
Ivan <75285470+techydad05@users.noreply.github.com>
2022-02-23 21:22:40 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 
 	kit: {
-		adapter: adapter(),
+		// adapter: adapter(),
+		adapter: vercel(options),
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
