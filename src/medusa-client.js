230deaa4f2df862406b7575e3a3655b1b9b1074d
Ivan <75285470+techydad05@users.noreply.github.com>
2022-02-23 21:31:15 -0500

    adding back Medusa

--- src/medusa-client.js
+++ src/medusa-client.js
@@ -1,37 +1,37 @@
-// import MedusaModule from "@medusajs/medusa-js";
-// const Medusa = MedusaModule.default;\
-// import Medusa from "@medusajs/medusa-js";
+import Medusa from "@medusajs/medusa-js";
 
-// const BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL || "http://localhost:9000";
+const BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL || "http://localhost:9000";
 
-// export const createClient = () => new Medusa({ baseUrl: BACKEND_URL });
+console.log(Medusa);
 
-// export const client = createClient();
+export const createClient = () => new Medusa({ baseUrl: BACKEND_URL });
 
-// console.log("client:", client);
+export const client = createClient();
 
-// export const getProducts = async () => {
-//   const { products } = await client.products.list();
-//   console.log("products", products);
-//   console.log("client", client);
-//   return products;
-// };
+console.log("client:", client);
 
-// export const getProduct = async (productId) => {
-//   const { product } = await client.products.retrieve(productId);
-//   console.log("product", product);
-//   return product;
-// };
+export const getProducts = async () => {
+  const { products } = await client.products.list();
+  console.log("products", products);
+  console.log("client", client);
+  return products;
+};
 
-// export const getCart = async () => {
-//   const { cart } = localStorage.cart_id ? await client.carts.retrieve(localStorage.cart_id) : await client.carts.create();
-//   localStorage.setItem("cart_id", cart.id);
-//   return cart;
-// }
+export const getProduct = async (productId) => {
+  const { product } = await client.products.retrieve(productId);
+  console.log("product", product);
+  return product;
+};
 
-// export const addToCart = async (variant, quantity) => {
-//   client.carts.lineItems.create(localStorage.cart_id, {
-//     variant_id: variant,
-//     quantity
-//   }).then(cart => console.log("added!", cart));
-// }
+export const getCart = async () => {
+  const { cart } = localStorage.cart_id ? await client.carts.retrieve(localStorage.cart_id) : await client.carts.create();
+  localStorage.setItem("cart_id", cart.id);
+  return cart;
+}
+
+export const addToCart = async (variant, quantity) => {
+  client.carts.lineItems.create(localStorage.cart_id, {
+    variant_id: variant,
+    quantity
+  }).then(cart => console.log("added!", cart));
+}
